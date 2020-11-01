1d32ef1ebfc835e5f01eb2f6fba723a588f75f41
Azael598 <987819317@qq.com>
2020-11-01 15:05:06 +0800

    logs and timeConsumeTask

--- src/com/john/diytomcat/Bootstrap.java
+++ src/com/john/diytomcat/Bootstrap.java
@@ -1,27 +1,37 @@
 package com.john.diytomcat;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.NetUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.log.LogFactory;
+import cn.hutool.system.SystemUtil;
 import com.john.diytomcat.http.Request;
 import com.john.diytomcat.http.Response;
 import com.john.diytomcat.util.Constant;
+import org.jsoup.internal.StringUtil;
 import sun.swing.StringUIClientPropertyKey;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Bootstrap {
     public static void main(String[] args) {
         try{
+            logJVM();
             int port=18080;
-            if (!NetUtil.isUsableLocalPort(port)){
-                System.out.println("port has already been used");
-                return;
-            }
+//            if (!NetUtil.isUsableLocalPort(port)){
+//                System.out.println("port has already been used");
+//                return;
+//            }
             ServerSocket ss = new ServerSocket(port);
             while (true){
                 Socket s = ss.accept();
@@ -37,10 +47,28 @@ public class Bootstrap {
 
                 //Refactor the response module
                 Response response = new Response();
-                String html = "Hello DIY Tomcat";
-                response.getWriter().println(html);
+                String uri = request.getUri();
+                if(null==uri){
+                    continue;
+                }
+                System.out.println(uri);
+                if ("/".equals(uri)){
+                    String html = "Hello DIY Tomcat";
+                    response.getWriter().println(html);
+                }else {
+                    String fileName = StrUtil.removePrefix(uri, "/");
+                    File file = FileUtil.file(Constant.rootFolder,fileName);
+                    if (file.exists()){
+                        String fileContent = FileUtil.readUtf8String(file);
+                        response.getWriter().println(fileContent);
+                        if (fileName.equals("timeConsume.html")){
+                            ThreadUtil.sleep(1000);
+                        }
+                    }else {
+                        response.getWriter().println("File Not Found");
+                    }
+                }
                 handle200(s,response);
-
 //                OutputStream os = s.getOutputStream();
 //                String response_head = "HTTP/1.1 200 OK\r\n"+"Content-Type: text/html\r\n\r\n";
 //                String responseString="Hello DIY Tomcat";
@@ -52,6 +80,28 @@ public class Bootstrap {
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    private static void logJVM(){
+        Map<String,String> infos = new LinkedHashMap<>();
+        infos.put("Server version","DIYTomcat/1.0.0");
+        infos.put("Server built","2020-11-1 14:11:00");
+        infos.put("Server number","1.0.0");
+        infos.put("OS Name\t", SystemUtil.get("os.name"));
+        infos.put("OS Version",SystemUtil.get("os.version"));
+        infos.put("Architecture",SystemUtil.get("os.arch"));
+        infos.put("Java Home",SystemUtil.get("java.home"));
+        infos.put("JVM Version",SystemUtil.get("java.runtime.version"));
+        infos.put("JVM Vendor",SystemUtil.get("java.vm.specification.vendor"));
+
+        Set<String> keys = infos.keySet();
+        for(String key : keys){
+            LogFactory.get().info(key+":\t\t"+infos.get(key));
+        }
+        //lambda
+//        infos.forEach((k,v)->{
+//            LogFactory.get().info(k+":\t\t"+infos.get(k));
+//        });
     }
 
     private static void handle200(Socket s,Response response) throws IOException{
--- src/com/john/diytomcat/test/TestTomcat.java
+++ src/com/john/diytomcat/test/TestTomcat.java
@@ -1,11 +1,20 @@
 package com.john.diytomcat.test;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.util.NetUtil;
 import cn.hutool.core.util.StrUtil;
 import com.john.diytomcat.util.MiniBrowser;
+import org.jsoup.helper.DataUtil;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class TestTomcat {
     private static int port=18080;
@@ -24,6 +33,29 @@ public class TestTomcat {
     public void testHelloTomcat(){
         String html = getContentString("/");
         Assert.assertEquals(html,"Hello DIY Tomcat");
+    }
+    @Test
+    public void testaHtml(){
+        String html = getContentString("/a.html");
+        Assert.assertEquals(html,"Hello DIY Tomcat from a.html");
+    }
+    @Test
+    public void testTimeConsumeHtml() throws InterruptedException{
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(20,20 ,60 , TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(10));
+        TimeInterval timeInterval = DateUtil.timer();
+
+        for(int i=0;i<3;i++){
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    getContentString("/timeConsume.html");
+                }
+            });
+        }
+        threadPool.shutdown();
+        threadPool.awaitTermination(1,TimeUnit.HOURS);
+        long duration = timeInterval.intervalMs();
+        Assert.assertTrue(duration>3000);
     }
     private String getContentString(String uri){
         String url= StrUtil.format("http://{}:{}{}",ip,port,uri);
